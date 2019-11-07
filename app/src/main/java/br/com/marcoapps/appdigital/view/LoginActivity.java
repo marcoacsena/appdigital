eab6507d8b0e96bede2dbb6cd7cb0f2341310d3d
marcoacsena <marcoacsenagmail.com>
2019-11-07 13:28:46 -0300

    Initial commit

---
+++ app/src/main/java/br/com/marcoapps/appdigital/view/LoginActivity.java
@@ -0,0 +1,16 @@
+package br.com.marcoapps.appdigital.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.marcoapps.appdigital.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

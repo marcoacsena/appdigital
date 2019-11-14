ce70b72c4782534136b03fc258cf6e1f2ac7c86f
marcoacsena <marcoacsenagmail.com>
2019-11-14 16:15:47 -0300

    Construção da quarta tela!

--- app/src/main/java/br/com/marcoapps/appdigital/view/NotificacaoActivity.java
+++ app/src/main/java/br/com/marcoapps/appdigital/view/NotificacaoActivity.java
@@ -2,11 +2,27 @@ package br.com.marcoapps.appdigital.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import br.com.marcoapps.appdigital.R;
 
 public class NotificacaoActivity extends AppCompatActivity {
+
+    private Activity activity;
+    private Button btnVoltarTelaListaDeMensagens;
+    private ImageView ivLogoEmissorDoDocumento;
+    private TextView tvTituloDocumento;
+    private TextView tvNumeroDocumento, tvNumeroProcesso;
+    private TextView tvCidadeEmissor, tvUF, tvDataDocumento;
+    private TextView tvTratamentoCliente, tvNomeCliente, tvSobreNomeCliente, tvCpfCnpj, tvNumeroCpfCnpj;
+    private TextView tvcorpoDoDocumento;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

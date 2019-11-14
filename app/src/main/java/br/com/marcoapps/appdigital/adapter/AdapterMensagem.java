ddf36d45c32a0cbd76f9def3d8356bbf0a398bff
marcoacsena <marcoacsenagmail.com>
2019-11-13 21:15:45 -0300

    Construção do RecyclerView para Emissores!

--- app/src/main/java/br/com/marcoapps/appdigital/adapter/AdapterMensagem.java
+++ app/src/main/java/br/com/marcoapps/appdigital/adapter/AdapterMensagem.java
@@ -4,69 +4,65 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.Adapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 import br.com.marcoapps.appdigital.R;
 import br.com.marcoapps.appdigital.model.Mensagem;
 
-public class AdapterMensagem extends BaseAdapter {
+public class AdapterMensagem extends RecyclerView.Adapter<AdapterMensagem.MyViewHolder> {
 
     private LayoutInflater inflater;
-    private List<Mensagem> listMensagens;
+    private List<Mensagem> listaDeMensagens;
     private Context context;
 
-    public AdapterMensagem(List<Mensagem> listMensagens, Context context) {
-        this.listMensagens = listMensagens;
-        this.context = context;
-        this.inflater = LayoutInflater.from(context);
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View itemListaMensagensEmissores = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_listview_emissores, parent, false);
+
+        return new MyViewHolder(itemListaMensagensEmissores);
     }
 
     @Override
-    public int getCount() {
-        return listMensagens.size();
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.ivEmissor.setImageResource(R.drawable.logo_santander);
+        holder.ivEnvelopeFechadoSantander.setImageResource(R.drawable.envelope_fechado_santander);
+        holder.tvNuMensagens.setText(String.valueOf(15));
+
     }
 
     @Override
-    public Object getItem(int i) {
-        return listMensagens.get(i);
+    public int getItemCount() {
+        return 5;
     }
 
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
+    public class MyViewHolder extends RecyclerView.ViewHolder{
 
-//    public void add(Mensagem mensagem){
-//        listMensagens.add(mensagem);
-//        this.notifyDataSetChanged();
-//    }
+        ImageView ivEmissor;
+        ImageView ivEnvelopeFechadoSantander;
+        TextView tvNuMensagens;
 
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+       public MyViewHolder(@NonNull View itemView) {
+           super(itemView);
 
-        //Resgata a i-ésima mensagem da lista
-        final Mensagem mensagem = listMensagens.get(i);
+           ivEmissor = itemView.findViewById(R.id.ivEmissor);
+           ivEnvelopeFechadoSantander = itemView.findViewById(R.id.ivEnvelopeFechadoSantander);
+           tvNuMensagens = itemView.findViewById(R.id.tvNuMensagens);
 
-        //Resgata os elementos para saída dos dados
-        view = inflater.inflate(R.layout.item_listview_emissores, null);
+       }
+   }
 
-        //Resgata os elementos para saída dos dados
-        ImageView ivEmissor = view.findViewById(R.id.ivEmissor);
-        ImageView ivEnvelopeFechadoSantander = view.findViewById(R.id.ivEnvelopeFechadoSantander);
-        TextView tvNuMensagem = view.findViewById(R.id.tvNuMensagem);
-
-        //Manda os dados para o layout item_listview_emissores
-        ivEmissor.setImageResource(R.drawable.logo_santander);
-        ivEnvelopeFechadoSantander.setImageResource(R.drawable.envelope_fechado_santander);
-        tvNuMensagem.setText(mensagem.getNuMensagens());
-
-
-        return view;
-    }
 
 
 }
--- app/src/main/java/br/com/marcoapps/appdigital/view/RecyclerViewEmissoresActivity.java
+++ app/src/main/java/br/com/marcoapps/appdigital/view/RecyclerViewEmissoresActivity.java
@@ -1,20 +1,17 @@
 package br.com.marcoapps.appdigital.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.Button;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import br.com.marcoapps.appdigital.R;
 import br.com.marcoapps.appdigital.adapter.AdapterMensagem;
-import br.com.marcoapps.appdigital.model.Mensagem;
 
 public class RecyclerViewEmissoresActivity extends AppCompatActivity {
 
@@ -22,36 +19,32 @@ public class RecyclerViewEmissoresActivity extends AppCompatActivity {
     private RecyclerView rvEmissores;
     private Button btnVoltar;
 
-    private List<Mensagem> listMensagens;
-    private AdapterMensagem adapterMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler_emissores);
+        setContentView(R.layout.activity_recycler_view_emissores);
 
         rvEmissores = findViewById(R.id.rvEmissores);
         btnVoltar = findViewById(R.id.btnVoltar);
-        configListView();
 
+
+        //Criar lista de filmes
+        //criarListaDeFilmes();
+
+        //Configurar adapter
+
+        AdapterMensagem adapterMensagem = new AdapterMensagem();
+
+        //Configurar recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        rvEmissores.setLayoutManager(layoutManager);
+        rvEmissores.setHasFixedSize(true);
+        rvEmissores.setAdapter( adapterMensagem);
     }
 
-    private void configListView() {
-
-        listMensagens = new ArrayList<>();
-
-        listMensagens.add(new Mensagem("Santander", "7"));
-        listMensagens.add(new Mensagem("Santander", "2"));
-
-        adapterMensagem = new AdapterMensagem(listMensagens, getApplicationContext());
-        rvEmissores.setAdapter(adapterMensagem);
-
-    }
-
-
-    public void voltarTelaCpf(View v){
+    public void voltarTelaLogin(View v){
 
         this.finish();
     }
-
 }
